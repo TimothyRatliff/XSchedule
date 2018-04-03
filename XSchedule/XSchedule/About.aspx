c96e937f43f5b57719bb01a5f673882b43a5160d
Sonia Azad <sazad6323@gmail.com>
2018-04-03 00:46:09 -0500

    Created a class library called "XServer" and added it as a refererence to the "XScheduler" web project. Defined the  class "mytest" within the class library. Instantiated a "mytest" class in the "About.aspx" file for testing. It prints a string to the About page. Build with no errors.

--- XSchedule/XSchedule/About.aspx
+++ XSchedule/XSchedule/About.aspx
@@ -3,5 +3,6 @@
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <h2><%: Title %></h2>
     <h3>XSchedule is a work scheduling app, built by a team of computer science students in the Software Systems Development course at Louisiana State University.</h3>
-    <p>It's free!</p>
+    <p>It's free!<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </p>
 </asp:Content>
---
+++ XSchedule/XServer/mytest.cs
@@ -0,0 +1,35 @@
+﻿/* This is an example class to check that it works with the XSchedule Web Project
+ * It follows the tutorial here: https://msdn.microsoft.com/en-us/library/cc668164.aspx
+ * There is a Label on the About.aspx page that uses the mytest class */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XServer
+{
+    public class mytest
+    {
+        private int _age;
+        private string _name;
+
+        public mytest()
+        {
+            Age = 0;
+            Name = "Not available";
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+}
