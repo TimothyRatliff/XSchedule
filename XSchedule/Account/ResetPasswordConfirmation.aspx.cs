3c6702f4eabc64ca1801a75a7487a87bcb7c2d37
Sonia Azad <sazad6323@gmail.com>
2018-04-11 11:56:53 -0500

    Add project files.

---
+++ XSchedule/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace XSchedule.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ XSchedule/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XSchedule.Startup))]
+namespace XSchedule
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
